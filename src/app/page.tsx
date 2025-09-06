10abf501d1ba3e5f1e3e3ea6870083ff970eb445
Iord Neto <iordneto95@gmail.com>
2025-09-05 21:13:09 -0300

    style: increase max width of main container for improved layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { StepNavigation } from "@/components/navigation";
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100 p-8">
-      <div className="max-w-4xl mx-auto">
+      <div className="max-w-7xl mx-auto">
         <StepNavigation />
       </div>
     </div>
